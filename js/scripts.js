2718d87e9714f6b1ecd5145cf0cb700a51558883
Caio <caiosilvaazeredo@hotmail.com>
2020-09-06 07:27:21 -0300

    parte de empresas

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+$( document ).ready(function() {
+
+  
+  // Filtro portfólio
+
+  $('.filter-btn').on('click', function() {
+
+    let type = $(this).attr('id');
+    let boxes = $('.project-box');
+
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+    if(type == 'dsg-btn') {
+      eachBoxes('dsg', boxes);
+    } else if(type == 'dev-btn') {
+      eachBoxes('dev', boxes);
+    } else if(type == 'eng-btn') {
+      eachBoxes('eng', boxes);
+	} else if(type == 'psi-btn') {
+      eachBoxes('psi', boxes);
+    } else {
+      eachBoxes('all', boxes);
+    }
+
+  });
+
+  function eachBoxes(type, boxes) {
+
+    if(type == 'all') {
+      $(boxes).fadeIn();
+    } else {
+      $(boxes).each(function() {
+        if(!$(this).hasClass(type)) {
+          $(this).fadeOut('fast');
+        } else {
+          $(this).fadeIn();
+        }
+      });
+    }
+  }
+  
+  document.getElementById("open-popup-btn").addEventListener("click",function(){
+  document.getElementsByClassName("popup")[0].classList.add("active");
+});
+
+document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
+  document.getElementsByClassName("popup")[0].classList.remove("active");
+});
+  
+  
+});
